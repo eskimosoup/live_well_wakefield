5d20dfbe3e1f62739838f7418e7dd9ce935afb81
James Doyley <jdoyley@gmail.com>
2016-06-02 10:36:01 +0100

    add display toggle to optimadmin menu items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014131452) do
+ActiveRecord::Schema.define(version: 20160602093423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20151014131452) do
     t.integer  "link_id"
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
+    t.boolean  "display",                     default: true,  null: false
   end
 
   add_index "optimadmin_menu_items", ["link_id"], name: "index_optimadmin_menu_items_on_link_id", using: :btree
