ed439ba16d88e92b5eb83b8879927636ef0e326e
optimised <noreply@optimised.today>
2018-02-02 15:16:26 +0000

    Send from noreply my

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,14 +15,13 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.office365.com',
-    user_name: 'paul@wearemy.co.uk',
+    user_name: 'noreply@wearemy.co.uk',
     domain: 'wearemy.co.uk',
     password: ENV['MY_PASSWORD'],
     authentication: :login,
     port: 587,
     enable_starttls_auto: true
   }
-
 
   # Settings specified here will take precedence over those in config/application.rb.
 
