897ef6f078e13ecc3b984305e52bd2b79a30ac01
Paul <pauloptimised@users.noreply.github.com>
2018-04-17 09:50:07 +0100

    Add reCaptcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+# seo.optimisd@gmail.com
+Recaptcha.configure do |config|
+  config.site_key  = '6LdgsFMUAAAAAA-Ow3NPEl5A9fpVkLzk-m2om9Mc'
+  config.secret_key = '6LdgsFMUAAAAALdmiHn-1QNTOVLz-EQ9QTxsP6N-'
+end
