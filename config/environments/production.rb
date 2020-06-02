0e1718c9992cb822faf3f3fbf067921fdf024dde
pauloptimised <pauloptimised@users.noreply.github.com>
2020-06-02 19:42:25 +0100

    Port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
     authentication: :plain,
     user_name: 'noreply@optimised.today',
     password: ENV['NOREPLY_PASSWORD'],
-    enable_starttls_auto: false
+    enable_starttls_auto: false,
+    port: 587
   }
 
   # ActionMailer::Base.smtp_settings = {
